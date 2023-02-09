9e0fb267f25b1b71457fed14d81415d9e0d323af
sid <sidharth@divami.com>
2023-02-09 13:18:57 +0530

    routes work!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,23 @@ import { FormComponentComponent } from './form-component/form-component.componen
 import { ChildComponent } from './form-component/child/child.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NewComponent } from './new/new.component';
-
 const appRoutes: Routes = [
   {
-    path: '/form',
+    path: '',
+    component: NewComponent,
+  },
+  {
+    path: 'form',
     component: FormComponentComponent,
   },
 ];
 @NgModule({
-  declarations: [AppComponent, FormComponentComponent, ChildComponent, NewComponent],
-  imports: [ReactiveFormsModule, BrowserModule, RouterModule],
+  declarations: [AppComponent, FormComponentComponent, ChildComponent],
+  imports: [
+    ReactiveFormsModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
